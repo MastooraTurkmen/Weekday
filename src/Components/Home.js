a103db1f970b624c2168a470865c510681c7dba0
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-03 19:04:21 +0430

    adds about company and salary

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -21,7 +21,23 @@ const Home = () => {
                     companyName
                 } = item
                 return (
-                    
+                    <div key={jdUid} className="card">
+                        <header className="job">
+                            <div className="time-container">
+                                <span>⏳</span>
+                                <p className="time">Posted 10 days ago</p>
+                            </div>
+                            <div className="job-info">
+                                <img src={logoUrl} alt="logo" className="logo" />
+                                <div className="sub-info">
+                                    <h4 className="company">{companyName}</h4>
+                                    <h4 className="rol">{jobRole}</h4>
+                                    <h4 className="city">{location}</h4>
+                                </div>
+                            </div>
+                            <p className="salary">Estimated Salary: {minJdSalary} - {maxJdSalary} {salaryCurrencyCode} ✅</p>
+                        </header>
+                    </div>
                 )
             })) : (
                 <h2>No Jobs available for this category at the moment</h2>
