01e018c7281b2ed6421ebba31d8909736b393bff
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-03 18:53:32 +0430

    get search and isLoading from useFetch

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,6 +2,9 @@ import { useEffect, useState } from 'react'
 import useFetch from './useFetch';
 
 const Home = () => {
+    const { search, isLoading } = useFetch()
+
+    
     return (
         <div>Home</div>
     )
