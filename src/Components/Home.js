01857b6c6d55f78d5dd2a66c4d8403c6259e20c0
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-03 18:58:28 +0430

    get the data

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,9 +4,29 @@ import useFetch from './useFetch';
 const Home = () => {
     const { search, isLoading } = useFetch()
 
-    
     return (
-        <div>Home</div>
+        <div className="card-container">
+            {search.length > 0 ? (search.map((item) => {
+                const { jdUid,
+                    jdLink,
+                    jobDetailsFromCompany,
+                    maxJdSalary,
+                    minJdSalary,
+                    salaryCurrencyCode,
+                    location,
+                    minExp,
+                    maxExp,
+                    jobRole,
+                    logoUrl,
+                    companyName
+                } = item
+                return (
+                    
+                )
+            })) : (
+                <h2>No Jobs available for this category at the moment</h2>
+            )}
+        </div>
     )
 }
 
