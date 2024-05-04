94460fb07d26ea20228daba7174154aba21c7a8d
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-04 19:20:45 +0430

    data js for filter jobs

---
+++ src/data.js
@@ -0,0 +1,396 @@
+const enginears = [
+    {
+        value: 'Frontend',
+        label: 'Frontend'
+    },
+    {
+        value: 'Backend',
+        label: 'Backend'
+    },
+    {
+        value: 'Fullstak',
+        label: 'Fullstak'
+    },
+    {
+        value: 'IOS',
+        label: 'IOS'
+    },
+    {
+        value: 'Flutter',
+        label: 'Flutter'
+    },
+    {
+        value: 'React Native',
+        label: 'React Native'
+    },
+    {
+        value: 'Android',
+        label: 'Android'
+    },
+    {
+        value: 'Teach Lead',
+        label: 'Teach Lead'
+    },
+    {
+        value: 'Dev-Ops',
+        label: 'Dev-Ops'
+    },
+    {
+        value: 'Data Engineer',
+        label: 'Data Engineer'
+    },
+    {
+        value: 'Data Sience',
+        label: 'Data Sience'
+    },
+    {
+        value: 'computer-vision',
+        label: 'computer-vision'
+    },
+    {
+        value: 'Nlp',
+        label: 'Nlp'
+    },
+    {
+        value: 'Deep Learning',
+        label: 'Deep Learning'
+    },
+    {
+        value: 'Test / Qa',
+        label: 'Test / Qa'
+    },
+    {
+        value: 'Web3',
+        label: 'Web3'
+    },
+    {
+        value: 'Sre',
+        label: 'Sre'
+    },
+    {
+        value: 'Data-Infrastruction',
+        label: 'Data-Infrastruction'
+    }
+
+
+
+]
+
+const Design = [
+    {
+        value: 'Designer',
+        label: 'Designer'
+    },
+    {
+        value: 'Design Manager',
+        label: 'Design Manager'
+    },
+    {
+        value: 'Graphic Designer',
+        label: 'Graphic Designer'
+    },
+    {
+        value: 'Product Designer',
+        label: 'Product Designer'
+    }
+
+]
+
+const product = [
+    {
+        value: 'Product Manager',
+        label: 'Product Manager'
+    }
+]
+
+const operations = [
+    {
+        value: 'Operations Manager',
+        label: 'Operations Manager'
+    },
+    {
+        value: `Founder's Office / Chief Of Staff`,
+        label: `Founder's Office / Chief Of Staff`
+    }
+]
+
+const sales = [
+    {
+        value: 'Sales Development Representativ',
+        label: 'Sales Development Representativ'
+    },
+    {
+        value: 'Account Executive',
+        label: 'Account Executive'
+    },
+    {
+        value: 'Account Manager',
+        label: 'Account Manager'
+    }
+]
+
+
+const marketing = [
+    {
+        value: 'Digital Marketing Manager',
+        label: 'Digital Marketing Manager'
+    },
+    {
+        value: 'Growth Hacker',
+        label: 'Growth Hacker'
+    },
+    {
+        value: 'Marketing',
+        label: 'Marketing'
+    },
+    {
+        value: 'Product Marketing Manager',
+        label: 'Product Marketing Manager'
+    }
+]
+
+
+const otherEngineering = [
+    {
+        value: 'HardWare',
+        label: 'HardWare'
+    },
+    {
+        value: 'Mechanical',
+        label: 'Mechanical'
+    },
+    {
+        value: 'Systems',
+        label: 'Systems'
+    }
+]
+
+const businessAnalyst = [
+    {
+        value: 'Business Analyst',
+        label: 'Business Analyst'
+    },
+]
+
+
+const projectManager = [
+    {
+        value: 'Project Manager',
+        label: 'Project Manager'
+    }
+]
+
+const management = [
+    {
+        value: 'Management',
+        label: 'Management'
+    }
+]
+
+const legal = [
+    {
+        value: 'Legal',
+        label: 'Legal'
+    }
+]
+
+const hr = [
+    {
+        value: 'Hr',
+        label: 'Hr'
+    }
+]
+
+const finance = [
+    {
+        value: 'Finance',
+        label: 'Finance'
+    }
+]
+
+
+export const Roles = [
+    {
+        label: 'enginears',
+        options: enginears
+    },
+    {
+        label: 'Design',
+        options: Design
+    },
+    {
+        label: 'product',
+        options: product
+    },
+    {
+        label: 'operations',
+        options: operations
+    },
+    {
+        label: 'sales',
+        options: sales
+    },
+    {
+        label: 'marketing',
+        options: marketing
+    },
+    {
+        label: 'other Engineering',
+        options: otherEngineering
+    },
+    {
+        label: 'Business Analyst',
+        options: businessAnalyst
+    },
+    {
+        label: 'project Manager',
+        options: projectManager
+    },
+    {
+        label: 'management',
+        options: management
+    },
+    {
+        label: 'legal',
+        options: legal
+
+    },
+    {
+        label: 'HR',
+        options: hr
+    },
+    {
+        label: 'finance',
+        options: finance
+    }
+
+]
+
+export const employesNum = [
+    {
+        value: '1-10',
+        label: '1-10'
+    },
+    {
+        value: '11-20',
+        label: '11-20'
+    },
+    {
+        value: '21-50',
+        label: '21-50'
+    },
+    {
+        value: '51-100',
+        label: '51-100'
+    },
+    {
+        value: '101-200',
+        label: '101-200'
+    },
+    {
+        value: '201-500',
+        label: '201-500'
+    },
+    {
+        value: '500+',
+        label: '500+'
+    }
+]
+
+
+export const experience = [
+    {
+        value: '1',
+        label: '1'
+    },
+    {
+        value: '2',
+        label: '2'
+    },
+    {
+        value: '3',
+        label: '3'
+    },
+    {
+        value: '4',
+        label: '4'
+    },
+    {
+        value: '5',
+        label: '5'
+    },
+    {
+        value: '6',
+        label: '6'
+    },
+    {
+        value: '7',
+        label: '7'
+    },
+    {
+        value: '8',
+        label: '8'
+    },
+    {
+        value: '9',
+        label: '9'
+    },
+    {
+        value: '10',
+        label: '10'
+    }
+]
+
+export const remote = [
+    {
+        value: 'Remote',
+        label: 'Remote'
+    },
+    {
+        value: 'Hybrid',
+        label: 'Hybrid'
+    },
+    {
+        value: 'In-office',
+        label: 'In-office'
+    }
+]
+
+export const minJdSalary = [
+    {
+        value: '0 USD',
+        label: '0 USD'
+    },
+    {
+        value: '10 USD',
+        label: '10 USD'
+    },
+    {
+        value: '20 USD',
+        label: '20 USD'
+    },
+    {
+        value: '30 USD',
+        label: '30 USD'
+    },
+    {
+        value: '40 USD',
+        label: '40 USD'
+    },
+    {
+        value: '50 USD',
+        label: '50 USD'
+    },
+    {
+        value: '60 USD',
+        label: '60 USD'
+    },
+    {
+        value: '70 USD',
+        label: '70 USD'
+    },
+    {
+        value: '100 USD',
+        label: '100 USD'
+    },
+]
