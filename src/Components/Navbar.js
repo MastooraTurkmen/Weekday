023813ee2d32fcfb2a50629c7e6951943c68ae66
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-04 19:20:10 +0430

    filter the data based on selection

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -15,6 +15,66 @@ const Navbar = ({ updateFilteredData }) => {
     const [loading, setLoading] = useState(true);
     const [input, setInput] = useState("") // set the input empty
 
+    useEffect(() => {
+        if (search) {
+            setLoading(false);
+        }
+    }, [search]);
+
+    const filterData = useCallback(() => {
+        // Filter the search based on all the select values
+        const filteredSearch = search.filter((item) => {
+            // Check if the jobRole is included in the select value
+            if (select && select.length > 0) {
+                const selectedJobs = select.map((item) => item.value.toLowerCase());
+                if (!selectedJobs.includes(item.jobRole.toLowerCase())) {
+                    return false;
+                }
+            }
+
+            // if the minExp matches the select value
+            if (select3) {
+                if (item.minExp !== parseInt(select3.value)) {
+                    return false;
+                }
+            }
+
+            // if the location matches the select value
+            if (select4 && select4.length > 0) {
+                if (select4[0].value === 'Remote' && item.location !== 'remote') {
+                    return false;
+                }
+                if (select4[0].value === 'In-office' && item.location === 'remote') {
+                    return false;
+                }
+            }
+
+            // Check if the minJdSalary matches the select value
+            if (select5 && select5[0]) {
+                if (`${item.minJdSalary} ${item.salaryCurrencyCode}` !== (select5[0] && select5[0].value)) {
+                    return false;
+                }
+            }
+
+            // Check if the companyName includes the input value
+            if (input) {
+                if (!item.companyName.toLowerCase().includes(input.toLowerCase())) {
+                    return false;
+                }
+            }
+
+            return true;
+        });
+
+        updateFilteredData(filteredSearch);
+    }, [select, select3, select4, select5, input, search, updateFilteredData]);
+
+
+    // filter data when click to the select
+    useEffect(() => {
+        filterData()
+    }, [select, select3, select4, select5, input])
+
     const handleChange = (select) => {
         setSelect(select);
     }
