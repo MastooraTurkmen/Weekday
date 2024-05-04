771b74fc7eb99891ec66403aca6072dae2bdb228
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-04 19:19:08 +0430

    adds Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 
 function App() {
   return (
-    <Home />
+    <div>
+      <Navbar />
+      <Home />
+    </div>
   );
 }
 
