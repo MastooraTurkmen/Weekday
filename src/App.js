6ab55d08fbf6223b5c4b5a095d15df89a5248e28
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-04 19:22:49 +0430

    filter data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 
 function App() {
+  const [filteredData, setFilteredData] = useState([]);
+
+  // filter data when click the select types
+  const updateFilteredData = (data) => {
+    setFilteredData(data)
+  };
+
   return (
     <div>
-      <Navbar />
-      <Home />
+      <Navbar updateFilteredData={updateFilteredData} />
+      <Home filteredData={filteredData} />
     </div>
   );
 }
